fae50c2cdc5a24111352d7fc8be318df9fc918e0
Ken Chase <kenchase@kenchase.com>
2025-03-22 11:08:04 -0400

    March 22 updates part 1

--- functions.php
+++ functions.php
@@ -1,13 +1,9 @@
 <?php
-// Enqueues style.css on the front.
-if (! function_exists('agilus_enqueue_styles')) :
-    /**
-     * Enqueues style.css on the front.
-     *
-     * @since Agilus 1.0
-     *
-     * @return void
-     */
+
+/**
+ * Enqueues style.css on the front-end.
+ */
+if (! function_exists('agilus_enqueue_styles')) {
     function agilus_enqueue_styles()
     {
         wp_enqueue_style(
@@ -17,5 +13,25 @@ if (! function_exists('agilus_enqueue_styles')) :
             wp_get_theme()->get('Version')
         );
     }
-endif;
+}
 add_action('wp_enqueue_scripts', 'agilus_enqueue_styles');
+
+/**
+ * We use WordPress's init hook to make sure
+ * our blocks are registered early in the loading
+ * process.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/init/
+ */
+function ag_register_acf_blocks()
+{
+    /**
+     * We register our block's with WordPress's handy
+     * register_block_type();
+     *
+     * @link https://developer.wordpress.org/reference/functions/register_block_type/
+     */
+    register_block_type(__DIR__ . '/blocks/team-member');
+}
+// Here we call our tt3child_register_acf_block() function on init.
+// add_action('init', 'ag_register_acf_blocks');
