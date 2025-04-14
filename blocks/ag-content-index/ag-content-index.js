9ac5ef87e8a6a5846700fbe34575cfffb6dbaaef
Ken Chase <kenchase@kenchase.com>
2025-04-14 09:57:17 -0400

    April 14 updates

--- blocks/ag-content-index/ag-content-index.js
+++ blocks/ag-content-index/ag-content-index.js
@@ -17,14 +17,27 @@ function setupBlocks(blocks) {
 function setupNav(blocks) {
   blocks.forEach((block) => {
     const contentLinks = block.querySelectorAll('.ag-content-index__menu a');
+    // Set the first link as the current one
+    contentLinks[0].classList.add('is-current');
+    // Loop through each link
     contentLinks.forEach((link) => {
-      link.classList.remove('is-current');
+      // Create a new element
+      const el = document.createElement('i');
+      // Set the class name for the element
+      el.className = 'fa-solid fa-caret-right';
+      // Insert the span at the beginning of the anchor
+      link.insertBefore(el, link.firstChild);
+      // Add an event listener to the link
       link.addEventListener('click', (event) => {
         event.preventDefault();
-        link.classList.add('is-current');
+        // Remove 'is-current' class from all links in this block
+        contentLinks.forEach((item) => {
+          item.classList.remove('is-current');
+        });
+        // Add 'is-current' class to the clicked element (which is 'link')
+        event.currentTarget.classList.add('is-current');
         const targetId = link.getAttribute('href').substring(1);
         const targetBlock = block.querySelector(`.ag-content-index__content > .wp-block-group[id="${targetId}"]`);
-
         if (targetBlock) {
           const contentBlocks = block.querySelectorAll('.ag-content-index__content > .wp-block-group');
           contentBlocks.forEach((contentBlock) => {
--- blocks/ag-content-index/ag-content-index.php
+++ blocks/ag-content-index/ag-content-index.php
@@ -13,51 +13,17 @@
 $block_id = !empty($block['anchor']) ? $block['anchor'] : '';
 $class_name = 'wrapper-block ag-content-index';
 
-// Add alignment class
-if (!empty($block['className'])) {
-    $class_name .= ' ' . $block['className'];
-}
-
-// Add alignment class
-if (!empty($block['align'])) {
-    $class_name .= ' align' . $block['align'];
-}
-
 // Add block ID attribute
 $id_attr = $block_id ? ' id="' . esc_attr($block_id) . '"' : '';
 
-// Block styles
-$block_style = '';
-if (!empty($block['style'])) {
-    // Background and text colors are managed through block supports
-    if (!empty($block['style']['color']['background'])) {
-        $block_style .= 'background-color:' . $block['style']['color']['background'] . ';';
-    }
-    if (!empty($block['style']['color']['text'])) {
-        $block_style .= 'color:' . $block['style']['color']['text'] . ';';
-    }
-
-    // Handle spacing (margin and padding)
-    if (!empty($block['style']['spacing'])) {
-        $spacing = $block['style']['spacing'];
-
-        if (!empty($spacing['padding'])) {
-            foreach ($spacing['padding'] as $side => $value) {
-                $block_style .= 'padding-' . $side . ':' . $value . ';';
-            }
-        }
-
-        if (!empty($spacing['margin'])) {
-            foreach ($spacing['margin'] as $side => $value) {
-                $block_style .= 'margin-' . $side . ':' . $value . ';';
-            }
-        }
-    }
-}
-
-$style_attr = !empty($block_style) ? ' style="' . esc_attr($block_style) . '"' : '';
+// Get the block attributes (https://www.advancedcustomfields.com/resources/acf-blocks-using-get_block_wrapper_attributes/)
+$style_attrs = $is_preview ? ' ' : get_block_wrapper_attributes(
+    array(
+        'class' => $class_name,
+    )
+);
 ?>
 
-<div<?php echo $id_attr; ?> class="<?php echo esc_attr($class_name); ?>" <?php echo $style_attr; ?>>
+<div <?php echo ($id_attr); ?> <?php echo ($style_attrs); ?>>
     <InnerBlocks />
-    </div>
+</div>
